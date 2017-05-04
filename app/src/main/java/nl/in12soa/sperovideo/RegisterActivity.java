dde74a953b5255522885b0ea9707da36c8d2975f
Mick <mdepeinder@gmail.com>
2017-05-04 16:31:08 +0200

    Just some basic registration shit

---
+++ app/src/main/java/nl/in12soa/sperovideo/RegisterActivity.java
@@ -0,0 +1,18 @@
+package nl.in12soa.sperovideo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mickd on 4-5-2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
