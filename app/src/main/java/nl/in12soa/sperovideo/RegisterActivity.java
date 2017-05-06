01539a0490fdf8e9aef33af4859bf4b630e613c3
Mick <mdepeinder@gmail.com>
2017-05-06 17:14:45 +0200

    Register form kinda done

--- app/src/main/java/nl/in12soa/sperovideo/RegisterActivity.java
+++ app/src/main/java/nl/in12soa/sperovideo/RegisterActivity.java
@@ -2,6 +2,8 @@ package nl.in12soa.sperovideo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by mickd on 4-5-2017.
@@ -13,6 +15,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    //User gets registered here.
+    public void registerUser(View v){
+        Toast.makeText(getApplicationContext(), "Register clicked", Toast.LENGTH_LONG).show();
     }
 
 }
