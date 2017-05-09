eebcf3bfe10db4914750767559bf3063019ae6f2
WouterAvans <wven2@avans.nl>
2017-05-09 09:55:28 +0200

    login with volley

---
+++ app/src/main/java/nl/in12soa/sperovideo/LoginActivity.java
@@ -0,0 +1,81 @@
+package nl.in12soa.sperovideo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import nl.in12soa.sperovideo.Services.ApiService;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+
+    private Map<String, String> params = new HashMap<>();
+
+    public static String loginURL = "http://innosportlab.herokuapp.com/users/validate";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+    }
+
+    public void doLogin(View v)
+    {
+        if(email.getText().toString() != "" && password.getText().toString() != "")
+        {
+            params.put("email", email.getText().toString());
+            params.put("password", password.getText().toString());
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.POST, loginURL, new JSONObject(params), new Response.Listener<JSONObject>() {
+                        //If succesfull
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(getApplicationContext(), "Login succesful", Toast.LENGTH_LONG).show();
+                            params.clear();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            try{
+                                String parsedData = new String(error.networkResponse.data, "UTF-8");
+                                JSONObject obj = new JSONObject(parsedData);
+                                String message = obj.getString("message");
+                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                            } catch (UnsupportedEncodingException | JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+            //Add this JSON object request to the requestQueue of the api
+            ApiService.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Username and password can't be empty", Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/nl/in12soa/sperovideo/MainActivity.java
+++ app/src/main/java/nl/in12soa/sperovideo/MainActivity.java
@@ -22,30 +22,20 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    //    private EditText email;
-//    private EditText password;
-//    private Button loginButton;
     private Button afstandsbedieningButton;
 //    private Button analyseButton;
-//    private Button cameraButton;
+    private Button cameraButton;
     private Button registerButton;
     private Context context;
 //    private Button registerButton;
-//    private String emailString;
-//    private String passwordString;
-
-//    public static String loginURL = "http://innosportlab.herokuapp.com/auth/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        email = (EditText)findViewById(R.id.email);
-//        password = (EditText)findViewById(R.id.password);
-//        loginButton = (Button)findViewById(R.id.loginButton);
 //        analyseButton = (Button) findViewById(R.id.test_analyse);
-//        cameraButton = (Button) findViewById(R.id.test_cam);
+        cameraButton = (Button) findViewById(R.id.test_cam);
         afstandsbedieningButton = (Button) findViewById(R.id.nfcActivityButton);
         registerButton = (Button) findViewById(R.id.register_button);
 
@@ -65,13 +55,13 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(analyse);
 //            }
 //        });
-//        cameraButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent camera = new Intent(context, ServerListActivity.class);
-//                startActivity(camera);
-//            }
-//        });
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         afstandsbedieningButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -81,78 +71,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                emailString = email.getText().toString();
-//                passwordString = password.getText().toString();
-//
-//                AsyncLogin aSyncLogin = new AsyncLogin();
-//                aSyncLogin.execute();
-//            }
-//        });
     }
 
-//    class AsyncLogin extends AsyncTask<Void, Void, String> {
-//        @Override
-//        protected String doInBackground(Void... voids) {
-//
-//            String response = "";
-//
-//            try {
-//                URL url = new URL(loginURL);
-//                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//                httpURLConnection.setDoOutput(true);
-//                httpURLConnection.setRequestMethod("POST");
-//                httpURLConnection.setRequestProperty("Content-Type", "application/json");
-//                httpURLConnection.connect();
-//
-//                JSONObject jsonObject = new JSONObject();
-//                jsonObject.put("email", emailString);
-//                jsonObject.put("password", passwordString);
-//
-//                DataOutputStream dataOutputStream = new DataOutputStream(httpURLConnection.getOutputStream());
-//                dataOutputStream.writeBytes(jsonObject.toString());
-//                dataOutputStream.flush();
-//                dataOutputStream.close();
-//
-//                int responseCode = httpURLConnection.getResponseCode();
-//
-//                if (responseCode == HttpsURLConnection.HTTP_OK) {
-//                    String line;
-//                    BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-//                    while ((line = br.readLine()) != null) {
-//                        response += line;
-//                    }
-//
-//                    JSONObject responseJSON = new JSONObject(response.toString());
-//                    if (responseJSON.has("_id")) {
-//                        response = "success";
-//
-//                    } else {
-//                        response = "unknown";
-//                    }
-//                } else {
-//                    response = "failure";
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            return response;
-//        }
-
-//        @Override
-//        protected void onPostExecute(String result) {
-//            super.onPostExecute(result);
-//            if (result.equals("unknown")) {
-//                Toast.makeText(getApplicationContext(), "Unknown combination of email and password", Toast.LENGTH_LONG).show();
-//            } else if (result.equals("failure")) {
-//                Toast.makeText(getApplicationContext(), "Failed to log in...", Toast.LENGTH_LONG).show();
-//            } else if (result.equals("success")) {
-//                Toast.makeText(getApplicationContext(), "Login succeed!", Toast.LENGTH_LONG).show();
-//            }
-//        }
-    //}
 }
