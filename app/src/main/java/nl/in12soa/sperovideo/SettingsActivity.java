71f77ca94ce00ce9246db9999a282c294e72d13c
Mick <mdepeinder@gmail.com>
2017-05-25 18:21:11 +0200

    Some more stuff

--- app/src/main/java/nl/in12soa/sperovideo/SettingsActivity.java
+++ app/src/main/java/nl/in12soa/sperovideo/SettingsActivity.java
@@ -1,10 +1,13 @@
 package nl.in12soa.sperovideo;
 
+import android.content.SharedPreferences;
 import android.hardware.Camera;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -19,23 +22,31 @@ import nl.in12soa.sperovideo.Services.ActionBarService;
  * Created by mickd on 15-5-2017.
  */
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private Spinner resolutionSpinner;
     private Spinner fpsSpinner;
     private ArrayList<String> resolutionList = new ArrayList<>();
     private ArrayList<String> fpsList = new ArrayList<>();
+    public static final String PREFS = "Preferences";
+    public SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         ActionBarService.setActionBarTitle(R.string.settings, getSupportActionBar());
+
         resolutionSpinner = (Spinner) findViewById(R.id.resolution_spinner);
         fpsSpinner = (Spinner) findViewById(R.id.fps_spinner);
+
         getSupportedCameraResolutions();
+
         initializeResolutionList();
         initializeFpsList();
+
+        resolutionSpinner.setOnItemSelectedListener(this);
+        fpsSpinner.setOnItemSelectedListener(this);
     }
 
     private void getSupportedCameraResolutions(){
@@ -59,5 +70,22 @@ public class SettingsActivity extends AppCompatActivity {
         fpsList.add("120");
         ArrayAdapter<String> fpsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, fpsList);
         fpsSpinner.setAdapter(fpsAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch(parent.getId()){
+            case R.id.resolution_spinner:
+                System.out.println(parent.getSelectedItem().toString());
+                break;
+            case R.id.fps_spinner:
+                System.out.println(parent.getSelectedItem().toString());
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
