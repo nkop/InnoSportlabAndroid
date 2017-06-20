65c3cfaa3a881b81718d6659378d62c8b119b1b0
WouterAvans <wven2@avans.nl>
2017-06-20 11:19:00 +0200

    asking permissions

--- app/src/main/java/nl/in12soa/sperovideo/MainActivity.java
+++ app/src/main/java/nl/in12soa/sperovideo/MainActivity.java
@@ -1,8 +1,12 @@
 package nl.in12soa.sperovideo;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
@@ -10,10 +14,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int PERMISSION_ALL = 1;
+    private String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.ACCESS_FINE_LOCATION};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!hasPermissions(this, permissions)){
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_ALL);
+        }
 
         (findViewById(R.id.register_button)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +58,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
