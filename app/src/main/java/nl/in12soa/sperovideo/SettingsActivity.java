3f8f18bfd87a7b81ff677f069d057c92ef91029e
Mick <mdepeinder@gmail.com>
2017-06-22 00:00:35 +0200

    Settings fixed

--- app/src/main/java/nl/in12soa/sperovideo/SettingsActivity.java
+++ app/src/main/java/nl/in12soa/sperovideo/SettingsActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,7 +31,9 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
     private Spinner resolutionSpinner;
     private Spinner fpsSpinner;
     private ArrayList<String> resolutionList = new ArrayList<>();
+    private ArrayList<String> resolutionListValues = new ArrayList<>();
     private ArrayList<String> fpsList = new ArrayList<>();
+    private HashMap<String, String> resolutionMap = new HashMap<>();
     public static final String PREFS = "CameraSettings";
     public SharedPreferences preferences;
 
@@ -43,18 +46,18 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         resolutionSpinner = (Spinner) findViewById(R.id.resolution_spinner);
         fpsSpinner = (Spinner) findViewById(R.id.fps_spinner);
 
-        getSupportedCameraResolutions();
+//        getSupportedCameraResolutions();
 
         initializeResolutionList();
         initializeFpsList();
 
         preferences = getSharedPreferences(PREFS, 0);
-        String resolutionX = preferences.getString("resolutionX", null);
-        String resolutionY = preferences.getString("resolutionY", null);
+        String resolution_quality  = preferences.getString("resolution_quality", null);
         String fps = preferences.getString("fps", null);
-        if( resolutionX!= null && resolutionY != null){
-            String concattedResolution = resolutionX + "x" + resolutionY;
-            resolutionSpinner.setSelection(resolutionList.indexOf(concattedResolution));
+        if(resolution_quality!= null){
+            int index = resolutionListValues.indexOf(resolution_quality);
+            String indexShow = resolutionList.get(index);
+            resolutionSpinner.setSelection(resolutionList.indexOf(indexShow));
         }
         if(fps != null){
             fpsSpinner.setSelection(fpsList.indexOf(fps));
@@ -64,19 +67,27 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         fpsSpinner.setOnItemSelectedListener(this);
     }
 
-    private void getSupportedCameraResolutions(){
-        Camera camera = CameraViewActivity.getCameraInstance();
-        Camera.Parameters params = camera.getParameters();
-        List<Camera.Size> resolutions = params.getSupportedVideoSizes();
-        for(Camera.Size s: resolutions){
-            this.resolutionList.add(s.width + "x" + s.height);
-        }
-    }
+//    private void getSupportedCameraResolutions(){
+//        Camera camera = CameraViewActivity.getCameraInstance();
+//        Camera.Parameters params = camera.getParameters();
+//        List<Camera.Size> resolutions = params.getSupportedVideoSizes();
+//        for(Camera.Size s: resolutions){
+//            this.resolutionList.add(s.width + "x" + s.height);
+//        }
+//    }
 
     private void initializeResolutionList(){
+        resolutionList.add("Laag");
+        resolutionList.add("Gemiddeld");
+        resolutionList.add("Hoog");
+        resolutionListValues.add("0");
+        resolutionListValues.add("1");
+        resolutionListValues.add("2");
+        resolutionMap.put("Laag", "0");
+        resolutionMap.put("Gemiddeld", "1");
+        resolutionMap.put("Hoog", "2");
         ArrayAdapter<String> resolutionAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, resolutionList);
         resolutionSpinner.setAdapter(resolutionAdapter);
-
     }
 
     private void initializeFpsList(){
@@ -93,10 +104,13 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         switch(parent.getId()){
             case R.id.resolution_spinner:
                 String selectedResolution = parent.getSelectedItem().toString();
-                String[] resolutionValues = selectedResolution.split("x");
-
-                editor.putString("resolutionX", resolutionValues[0]);
-                editor.putString("resolutionY", resolutionValues[1]);
+                String resolutionValue = resolutionMap.get(selectedResolution);
+                editor.putString("resolution_quality", resolutionValue);
+//                String selectedResolution = parent.getSelectedItem().toString();
+//                String[] resolutionValues = selectedResolution.split("x");
+//
+//                editor.putString("resolutionX", resolutionValues[0]);
+//                editor.putString("resolutionY", resolutionValues[1]);
 
                 Toast.makeText(getApplicationContext(), "Resolutie opgeslagen", Toast.LENGTH_LONG).show();
                 break;
